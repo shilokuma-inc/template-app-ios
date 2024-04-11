ecc169552af1333c4d4456cc20d50ee0296d712b
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-11 23:01:25 +0900

    make template file

---
+++ iOS-App-Template/iOS-App-Template/iOS_App_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_TemplateApp.swift
+//  iOS-App-Template
+//
+//  Created by 村石 拓海 on 2024/04/11.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
