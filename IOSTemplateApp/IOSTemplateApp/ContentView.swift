f1b2fe6e336a7bcd3918ecb2bc026d0831927b40
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-12 23:08:56 +0900

    [chore] make project IOSTemplateApp

---
+++ IOSTemplateApp/IOSTemplateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSTemplateApp
+//
+//  Created by 村石 拓海 on 2024/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSTemplateApp/IOSTemplateApp/IOSTemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTemplateAppApp.swift
+//  IOSTemplateApp
+//
+//  Created by 村石 拓海 on 2024/05/12.
+//
+
+import SwiftUI
+
+@main
+struct IOSTemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
